d7c2371da33150951706c9dbcae574a0ce586ff8
Mouhamadou Mactar CAMARA <camaramouhamadoumactar@gmail.com>
2026-04-27 21:01:32 +0000

    [Mactar] CRUD correction du fichier routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\MenuController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\Admin\MenuController;
 
-// Menus (à l'intérieur du groupe admin existant)
-Route::get('menus/archives', [MenuController::class, 'archives'])->name('menus.archives');
-Route::post('menus/{id}/restore', [MenuController::class, 'restore'])->name('menus.restore');
-Route::resource('menus', MenuController::class);
+// Routes admin
+Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->group(function () {
+
+    // Tables
+    Route::get('tables/archives', [TableController::class, 'archives'])->name('tables.archives');
+    Route::post('tables/{id}/restore', [TableController::class, 'restore'])->name('tables.restore');
+    Route::resource('tables', TableController::class);
+
+    // Menus
+    Route::get('menus/archives', [MenuController::class, 'archives'])->name('menus.archives');
+    Route::post('menus/{id}/restore', [MenuController::class, 'restore'])->name('menus.restore');
+    Route::resource('menus', MenuController::class);
+
+});
